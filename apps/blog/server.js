c9c1db6454b98f02911661f56be00bb29f688490
Eugene <0ggy.sin@gmail.com>
2022-01-03 15:06:02 +0200

    chore(blog): scaffold next lib blog

---
+++ apps/blog/server.js
@@ -0,0 +1,46 @@
+// @ts-check
+'use strict';
+
+/**
+ * Production Nextjs custom server
+ *
+ * Usage: run this script with node
+ * Adjust dir option for your serve/deploy config
+ *
+ * node server.js
+ */
+
+/**
+ * @typedef {import(next/dist/server/next-server).default} Server
+ */
+
+const NextServer = require(next / dist / server / next - server).default;
+const express = require('express');
+
+const nextApp = new NextServer({
+  dir: './dist/apps/<%= name %>',
+  staticMarkup: false,
+  quiet: false,
+  conf: {
+    distDir: '.',
+  },
+});
+
+const serve = async () => {
+  const handle = nextApp.getRequestHandler();
+  const expressApp = express();
+
+  await nextApp.prepare();
+
+  return new Promise((resolve, reject) => {
+    expressApp.all('*', (req, res) => {
+      return handle(req, res);
+    });
+
+    const server = expressApp.listen(4200, (err) => {
+      err ? reject(err) : resolve(server);
+    });
+  });
+};
+
+serve().then((server) => console.log('Server is running on port 4200'));
