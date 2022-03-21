d896bf4bf8483c924ca86f87f81011eba39dd0bb
Eugene <0ggy.sin@gmail.com>
2022-03-21 09:39:09 +0200

    feat(research): add basic description on index

--- apps/blog/pages/research/index.tsx
+++ apps/blog/pages/research/index.tsx
@@ -3,8 +3,31 @@ export interface ResearchProps {}
 
 export default function Research(props: ResearchProps) {
   return (
-    <div>
-      <h3>Welcome to Research!</h3>
-    </div>
+    <>
+      <h3>
+        What if I tell you, that you can clean your code base, just by learning
+        a few mind trick?
+      </h3>
+
+      <h4>
+        I am researching a combination of a list of small refactoring techniques
+        and psychological practices, to inspire developers to perpetually
+        increase the code quality with micro steps.
+      </h4>
+
+      <h5>The list of useful facts and ideas:</h5>
+
+      <ul>
+        <li>Millers law: 7 +/- 2 things can be kept in focus at same time.</li>
+        <li>
+          Pomodoro techniques: customize the timer to fit you personality.
+        </li>
+        <li>Marty Lobdell: associative principles to memorize facts.</li>
+        <li>
+          Dr. Bruce Lipton: retrain your subconsciousness to automate refactor.
+        </li>
+        <li>Pareidolia: tendency interpret noise as things.</li>
+      </ul>
+    </>
   );
 }
