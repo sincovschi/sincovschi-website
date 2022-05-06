41c244b44a57523e5eedf7ff7098215a014efebc
Eugene <0ggy.sin@gmail.com>
2022-05-06 12:40:59 +0300

    feat(research): :lipstick: separate markdown from components

---
+++ apps/blog/pages/research/index.md
@@ -0,0 +1,16 @@
+# What if I tell you, that you can clean your codebase, by learning a few mind tricks?
+
+---
+
+## Now, I am researching a combination of psychological ideas that will trigger automated refactoring. I want to help developers to perpetually improve existing code. As a result, to increase the world's software quality.
+
+---
+
+**The research list:**
+
+- Pareidolia: the tendency to impose meaning where there is none.
+- Pomodoro techniques: customize the timer to fit your personality.
+- Millers law: only 7 +/- 2 things can be kept in focus at the same time.
+- Marty Lobdell: associative principles to memorize facts.
+- Dr. Bruce Lipton: retrain your subconsciousness to automate refactor.
+- Atomic habits: a rewarding routine or regular practice.
--- apps/blog/pages/research/index.tsx
+++ apps/blog/pages/research/index.tsx
@@ -1,33 +1,42 @@
-/* eslint-disable-next-line */
-export interface ResearchProps {}
+import { marked } from 'marked';
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { join } from 'path';
+import fs from 'fs';
+
+export interface ResearchProps {
+  htmlString: string;
+}
 
 export default function Research(props: ResearchProps) {
   return (
     <>
-      <h3>
-        What if I tell you, that you can clean your code base, just by learning
-        a few mind trick?
-      </h3>
+      <Head>
+        <title>Eugene&apos;s Research.</title>
+        <meta
+          name="description"
+          content="A combination of psychological ideas that will trigger automated refactoring"
+        />
+      </Head>
 
-      <h4>
-        I am researching a combination of a list of small refactoring techniques
-        and psychological practices, to inspire developers to perpetually
-        increase the code quality with micro steps.
-      </h4>
-
-      <h5>The list of useful facts and ideas:</h5>
-
-      <ul>
-        <li>Millers law: 7 +/- 2 things can be kept in focus at same time.</li>
-        <li>
-          Pomodoro techniques: customize the timer to fit you personality.
-        </li>
-        <li>Marty Lobdell: associative principles to memorize facts.</li>
-        <li>
-          Dr. Bruce Lipton: retrain your subconsciousness to automate refactor.
-        </li>
-        <li>Pareidolia: tendency interpret noise as things.</li>
-      </ul>
+      <article dangerouslySetInnerHTML={{ __html: props.htmlString }} />
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps<{
+  htmlString: string;
+}> = async () => {
+  const filePath = join(
+    process.cwd(),
+    'apps',
+    'blog',
+    'pages',
+    'research',
+    'index.md'
+  );
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const htmlString = marked.parse(fileContent);
+
+  return { props: { htmlString } };
+};
