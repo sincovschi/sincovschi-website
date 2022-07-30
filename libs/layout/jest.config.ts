74fa81c61462ddff99d3b8e8c2423bcb0c4e5f51
Eugene <0ggy.sin@gmail.com>
2022-07-30 11:00:55 +0200

    build(workspace): :arrow_up: migrate nx to latest

--- libs/layout/jest.config.ts
+++ libs/layout/jest.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   displayName: 'layout',
 
   globals: {
@@ -11,5 +11,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/layout',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
 };
